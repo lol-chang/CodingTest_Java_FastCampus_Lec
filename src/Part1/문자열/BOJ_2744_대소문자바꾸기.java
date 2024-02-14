811181e5b30e052d9b0f23fa292a4c869e0490c9
lol-chang <doclecsj@naver.com>
2024-02-15 01:58:34 +0900

    2744_대소문자 바꾸기 solve

--- src/Part1/문자열/BOJ_2744_대소문자바꾸기.java
+++ src/Part1/문자열/BOJ_2744_대소문자바꾸기.java
@@ -1,9 +1,12 @@
+package Part1.문자열;
+
 // [String Part - 1]
 // [BOJ - 2744] 대소문자 바꾸기
 // https://www.acmicpc.net/problem/2744
 
 import java.util.Scanner;
-public class Main {
+
+public class BOJ_2744_대소문자바꾸기 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
